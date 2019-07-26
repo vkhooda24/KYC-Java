6d2261ce94bc2cfbb0f7f2d5e238bc6b18ee5ffd
vk hooda <er.vkhooda24@gmail.com>
2019-07-25 23:32:52 -0400

    Clean up.

--- app/src/main/java/com/vkhooda24/knowyourcountry/ui/activities/WelcomeActivity.java
+++ app/src/main/java/com/vkhooda24/knowyourcountry/ui/activities/WelcomeActivity.java
@@ -1,11 +1,8 @@
 package com.vkhooda24.knowyourcountry.ui.activities;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import com.vkhooda24.knowyourcountry.R;
-import com.vkhooda24.knowyourcountry.constants.IntentKeys;
 
 /**
  * Created by Vikram Hooda on 1/3/19.
